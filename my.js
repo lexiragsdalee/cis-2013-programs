0025a392c0e2b7184d527ea9e315a6339f74f60f
lexiragsdalee <56270931+lexiragsdalee@users.noreply.github.com>
2021-09-03 09:56:38 -0500

    Add files via upload

---
+++ my.js
@@ -0,0 +1,22 @@
+var $ = function (id) 
+{
+    return document.getElementById(id);
+}
+
+var mainProgram = function () 
+{
+    var stringFirstName = prompt("What is your first name?");
+    if (!isNaN(stringFirstName)) {
+        alert("You did not enter a valid first name, I shall call you Squishy.");
+    } else
+    {
+        alert("Hello "+ stringFirstName +"! It's great to meet you!");  
+    }
+}
+
+window.onload = function () 
+{
+    $("click").onclick = mainProgram;
+    $("item_cost").focus(); //puts the cursor on the first DOM text input box
+}
+
