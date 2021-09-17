f4a73e099f632d9fe79f9b362e7a85a9575362a9
lexiragsdalee <56270931+lexiragsdalee@users.noreply.github.com>
2021-09-17 13:13:35 -0500

    Add files via upload

---
+++ ageCalculated.js
@@ -0,0 +1,8 @@
+var floatAge, floatDays, intWeeks, floatMonths, intFortnights;
+    floatAge = parseFloat(prompt("Enter your age"));
+    floatDays = floatAge * 365.25;
+    floatMonths = floatAge * 12;
+    intWeeks = floatDays / 7;
+    intFortnights = floatDays / 14;
+    alert("Your age in years is " + floatAge + ". " + " Your age in days is " + floatDays + ". " + "Your age in Months is " + floatMonths + ". " + "Your age in Weeks is " + intWeeks.toFixed(2) + ". " + "Your age in Fortnights is " + intFortnights.toFixed(2) + "." );
+    
